f58f1ef89e233cbb43823c400f24d5c8b885d6f1
EARives <errin.rives98@gmail.com>
2024-06-10 16:07:21 -0400

    fixed calculation class

--- calculator/calculation.py
+++ calculator/calculation.py
@@ -1,15 +1,9 @@
-from decimal import Decimal
-
 class Calculation:
-    """Class to perform basic calculations"""
-
     def __init__(self, a: Decimal, b: Decimal, operation):
         self.a = a
         self.b = b
         self.operation = operation
 
-    def perform(self):
-        """Perform the calculation"""
-        return self.operation(self.a, self.b)
-
+    def __repr__(self):
+        return f"Calculation({self.a}, {self.b}, {self.operation.__name__})"
 
