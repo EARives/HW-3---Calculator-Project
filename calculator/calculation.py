b06efd5cf65febed06f91e7adce30bb794c25e6b
EARives <errin.rives98@gmail.com>
2024-06-10 15:48:21 -0400

    updated file to contain annotations to pass other py tests

--- calculator/calculation.py
+++ calculator/calculation.py
@@ -1,14 +1,15 @@
-from typing import Callable
+from decimal import Decimal
 
 class Calculation:
-    def __init__(self, a: float, b: float, operation: Callable[[float, float], float]):
+    """Class to perform basic calculations"""
+
+    def __init__(self, a: Decimal, b: Decimal, operation):
         self.a = a
         self.b = b
         self.operation = operation
 
-    def perform(self) -> float:
+    def perform(self):
+        """Perform the calculation"""
         return self.operation(self.a, self.b)
 
-    def __repr__(self):
-        return f"Calculation({self.a}, {self.b}, {self.operation.__name__})"
 
