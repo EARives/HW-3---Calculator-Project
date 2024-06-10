bec3bedffdba77f36a3d5ff2f0afffc96f389702
EARives <errin.rives98@gmail.com>
2024-06-10 16:13:45 -0400

    reverted back to previous code

--- calculator/calculation.py
+++ calculator/calculation.py
@@ -1,9 +1,15 @@
+from decimal import Decimal
+
 class Calculation:
+    """Class to perform basic calculations"""
+
     def __init__(self, a: Decimal, b: Decimal, operation):
         self.a = a
         self.b = b
         self.operation = operation
 
-    def __repr__(self):
-        return f"Calculation({self.a}, {self.b}, {self.operation.__name__})"
+    def perform(self):
+        """Perform the calculation"""
+        return self.operation(self.a, self.b)
+
 
